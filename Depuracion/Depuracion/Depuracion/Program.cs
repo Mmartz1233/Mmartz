53b448e1dd63089c3bc3dc34fc3d5378b1b9407b
Mmartz1233 <Mmmartz123@hotmail.com>
2023-01-19 12:04:39 -0600

    En esta clase se ven los siguientes temas: 1. Como obtener los puntos de interrupccion en la depuracion. 2. Como obtener una pila de llamadas para ver el orden de ejecucion de un programa. 3. Como ver el valor de las variables locales y automaticas durante la depuracion. 4. Como crear nuestras propias excepciones.

---
+++ Depuracion/Depuracion/Depuracion/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Depuracion
+{
+    class Program
+    {
+        //Para ver el orden de ejecucion de un programa: Depurar> Ventana> Obtener pilla de llamadas
+        //Para ver los puntos de depuracion:  Depurar> Ventanas>Puntos de interrupccion
+        static void Main(string[] args)
+        {
+            // variable que almacena la lista completa de amigos
+           var amigos = new List<string> { "María", "Ana", "Martina", "Juan", "Leonardo", "Carlos", "Marianela"};
+            //var amigos = new List<string>();
+            // variable que contiene los amigos que irán a la fiesta
+            var amigosFiesta = ObtenerAmigosFiesta(amigos, 3);
+
+            // loop para mostrar en pantalla los nombres de los amigos que irán a la fiesta
+      
+
+            foreach (var nombre in amigosFiesta)
+                Console.WriteLine(nombre);
+            Console.Read();
+
+        }
+
+        /// <summary>
+        /// Método para cargar la lista de amigos que irán a la fiesta
+        /// </summary>
+        /// <param name="lista"></param>
+        /// <param name="cuenta"></param>
+        /// <returns></returns>
+        public static List<string> ObtenerAmigosFiesta(List<string> lista, int cuenta)
+        {
+            if (lista == null)
+                throw new ArgumentNullException("lista", "La lista no puede ser vacia");
+            //verificamos que tenga la cantidad de la cuenta no sea mas grande que los de la lista
+            if (cuenta > lista.Count || cuenta <= 0)
+                throw new ArgumentOutOfRangeException("Cuenta", "La cuenta no puede ser mas grande que la lista o menor a cero");
+
+            var amuleto = new List<string>(lista);
+            // variable que contendrá la lista de amigos
+            var amigosFiesta = new List<string>();
+
+            // Loop para cargar la lista hasta el límite de personas
+            while(amigosFiesta.Count < cuenta)
+            {
+                var amigoActual = ObtenerAmigoFiesta(amuleto);
+                amigosFiesta.Add(amigoActual);
+                amuleto.Remove(amigoActual);
+            }
+            return amigosFiesta;
+        }
+
+        // Método para elegir cada amigo que asistirá
+        public static string ObtenerAmigoFiesta(List<string> lista)
+        {
+            // Variable que contendrá el emigo con el nombre más corto
+            string nombreMasCorto = lista[0];
+            // Loop para revisar la lista y detectar al que tiene el nombre más corto
+            for(var i = 0; i<lista.Count; i++)
+            {
+                if(lista[i].Length > nombreMasCorto.Length)
+                {
+                    nombreMasCorto = lista[i];
+                }
+            }
+            return nombreMasCorto;
+        }
+    }
+}
---
+++ Depuracion/Depuracion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Security.Principal;
+//para la eliminacion de errores
+namespace Depuracion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ObtenerAmigosFiesta();
+            Console.Read();
+        }
+        public static void ObtenerAmigosFiesta()
+        {
+            var amigos = new List<string> {"Arvizu","Estefhania","Liz","Lu", "Irene", "Paola", "Iris", "Diego","kat"};
+            var listaNuevosAmigos = new List<string> { };
+            var nombremascorto = amigos[4];
+            Console.WriteLine("La cantidad de letras que tiene el nombre mas corto es: {0}",amigos[4].Length);
+            for (var i = 0; i< amigos.Count;i++)//Listar a todos los amigos
+            {
+                
+                if (amigos.Count < nombremascorto.Length)//si alguno de los amigos tiene el nombre mas corto que el amigo designado
+                {
+                    Console.WriteLine("El nombre mas corto es {0}", nombremascorto = amigos[i]); //asigna los nombres mas cortos a la lista general
+                    listaNuevosAmigos.Add(nombremascorto = amigos[i]);
+                    foreach (var a in listaNuevosAmigos)
+                    {
+
+                        Console.WriteLine("Los nuevos amigos invitados son: {0}", a);
+                    }
+                }
+            }
+           
+        }
+    }
+}
+ 
